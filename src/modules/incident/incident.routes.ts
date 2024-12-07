27ae0f74c0c64233a3ae0494a965c60027a87db9
ampomahkelvin <kevboat963@gmail.com>
2024-12-07 06:56:41 +0000

    Add user authorization middleware

--- src/modules/incident/incident.routes.ts
+++ src/modules/incident/incident.routes.ts
@@ -1,9 +1,14 @@
 import express from 'express'
 import { IncidentController } from './incident.controller'
+import { UserAuthMiddleware } from '../users/middlewares/auth.middleware'
 
 const router = express.Router()
 
-router.post('/submit', IncidentController.submitIncident)
+router.post(
+  '/submit',
+  UserAuthMiddleware.tokenGuard,
+  IncidentController.submitIncident,
+)
 router.get('/', IncidentController.getIncidents)
 router.get('/getBy/:countryName', IncidentController.getIncidentsByCountry)
 
