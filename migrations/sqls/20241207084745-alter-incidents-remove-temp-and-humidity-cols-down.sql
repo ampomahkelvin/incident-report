8b939732a9d19b9a418b1c4c82c123b0e1777923
ampomahkelvin <kevboat963@gmail.com>
2024-12-07 08:53:32 +0000

    Alter database table Removed columns temperature and humidity

---
+++ migrations/sqls/20241207084745-alter-incidents-remove-temp-and-humidity-cols-down.sql
@@ -0,0 +1,4 @@
+/* Replace with your SQL commands */
+ALTER TABLE "incidents"
+    ADD COLUMN "temperature" FLOAT,
+    ADD COLUMN "humidity" FLOAT;
---
+++ migrations/sqls/20241207084745-alter-incidents-remove-temp-and-humidity-cols-up.sql
@@ -0,0 +1,4 @@
+/* Replace with your SQL commands */
+ALTER TABLE "incidents"
+    DROP COLUMN "temperature",
+    DROP COLUMN "humidity";
