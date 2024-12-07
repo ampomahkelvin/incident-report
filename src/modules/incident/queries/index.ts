08d464878882b57f5fc83dc6555fbe2676c82dfa
ampomahkelvin <kevboat963@gmail.com>
2024-12-07 08:55:58 +0000

    change query fetching temperature and humidity from weather_report object now

--- src/modules/incident/queries/index.ts
+++ src/modules/incident/queries/index.ts
@@ -1,14 +1,14 @@
 const submitIncident = `
-INSERT INTO "incidents" ("incident_desc", "city", "country", "date", "weather_report", "temperature", "humidity", "client_id") VALUES ($1, $2, $3, $4, $5, $6, $7, $8) RETURNING *
+INSERT INTO "incidents" ("incident_desc", "city", "country", "date", "weather_report", "client_id") VALUES ($1, $2, $3, $4, $5, $6) RETURNING *
 `
 
 const getIncidents = `
 SELECT * FROM "incidents" WHERE 
     ($1 IS NULL OR "city" = $1) AND
-    ($2 IS NULL OR "temperature" >= $2) AND
-    ($3 IS NULL OR "temperature" <= $3) AND
-    ($4 IS NULL OR "humidity" >= $4) AND
-    ($5 IS NULL OR "humidity" <= $5) 
+    ($2 IS NULL OR ("weather_report" -> 'main' ->>'temp')::FLOAT >= $2) AND
+    ($3 IS NULL OR ("weather_report" -> 'main' ->>'temp')::FLOAT <= $3) AND
+    ($4 IS NULL OR ("weather_report" -> 'main' ->>'humidity')::FLOAT >= $4) AND
+    ($5 IS NULL OR ("weather_report" -> 'main' ->>'humidity')::FLOAT <= $5) 
 `
 
 const getIncidentByCountryName = `
