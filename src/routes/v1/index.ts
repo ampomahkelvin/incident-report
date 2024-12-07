eb88acebd05357b966d7459b662610b7b3aaf47e
ampomahkelvin <kevboat963@gmail.com>
2024-12-07 07:00:50 +0000

    Add user router

--- src/routes/v1/index.ts
+++ src/routes/v1/index.ts
@@ -1,8 +1,10 @@
 import express from 'express'
 import IncidentRouter from '../../modules/incident/incident.routes'
+import UserRouter from '../../modules/users/user.routes'
 
 const appRouter = express.Router()
 
 appRouter.use('/incidents', IncidentRouter)
+appRouter.use('/users', UserRouter)
 
 export const Router = appRouter
