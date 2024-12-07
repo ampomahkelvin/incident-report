8b0cb022102a4aa506b5598b12cb8ab8414b4d15
ampomahkelvin <kevboat963@gmail.com>
2024-12-07 08:56:11 +0000

    fix type error

--- src/modules/users/user.routes.ts
+++ src/modules/users/user.routes.ts
@@ -4,7 +4,7 @@ import { UserController } from './user.controller'
 const router = express.Router()
 
 router.post('/register', UserController.createUser as any)
-router.post('/login', UserController.loginUser)
+router.post('/login', UserController.loginUser as any)
 router.get('/', UserController.getAllUsers)
 
 export default router
